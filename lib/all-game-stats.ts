8a7b16c99a7f56014c2d4a12aecd5471d99c7fc5
varsityvue <info@varsityvue.com>
2026-09-05 17:16:45 -0500

    Merge supplemental Week 2 stats

---
+++ lib/all-game-stats.ts
@@ -0,0 +1,4 @@
+import { gameStats as baseGameStats } from "@/data/game-stats";
+import { week2GameStats } from "@/data/week2-game-stats";
+
+export const gameStats = [...baseGameStats, ...week2GameStats];
