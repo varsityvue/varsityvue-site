7dfcb5f74df43bf9e1cbf9683e7d23a5abacdfa0
varsityvue <info@varsityvue.com>
2026-09-06 12:37:56 -0500

    Include coach-provided game stats

--- lib/all-game-stats.ts
+++ lib/all-game-stats.ts
@@ -1,4 +1,9 @@
 import { gameStats as baseGameStats } from "@/data/game-stats";
 import { week2GameStats } from "@/data/week2-game-stats";
+import { coachGameStats } from "@/data/coach-game-stats";
 
-export const gameStats = [...baseGameStats, ...week2GameStats];
+export const gameStats = [
+  ...baseGameStats,
+  ...week2GameStats,
+  ...coachGameStats,
+];
