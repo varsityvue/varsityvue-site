deda5f76e20b47e263aa2acb74a19debe66e1fe2
varsityvue <info@varsityvue.com>
2026-09-05 22:52:48 -0500

    Use permanent redirect for legacy school request route

--- app/recommend-school/page.tsx
+++ app/recommend-school/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 
 export default function RecommendSchoolPage() {
-  redirect("/school-request");
+  permanentRedirect("/school-request");
 }
