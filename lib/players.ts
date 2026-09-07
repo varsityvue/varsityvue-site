a672ab6d264f6dcf637e9a5feb52ee053c0ee985
varsityvue <info@varsityvue.com>
2026-09-06 23:20:42 -0500

    Remove stale watch-player modules blocking build

--- lib/players.ts
+++
@@ -1,9 +0,0 @@
-import { watchPlayers } from "@/data/players";
-
-export function getFeaturedPlayers() {
-    return watchPlayers.filter((player) => player.featured);
-}
-
-export function getPlayersForSchool(schoolSlug: string) {
-    return watchPlayers.filter((player) => player.schoolSlug === schoolSlug);
-}
