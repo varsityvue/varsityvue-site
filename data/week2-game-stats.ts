7f778e31b524d7a0df38f122647a2dd6338552d9
varsityvue <info@varsityvue.com>
2026-09-08 08:52:08 -0500

    Correct Stamford Week 2 player name

--- data/week2-game-stats.ts
+++ data/week2-game-stats.ts
@@ -28,7 +28,7 @@ export const week2GameStats: GameStats[] = [
     ],
     rushing: [
       { player: "Lane Couch", schoolSlug: "de-leon", attempts: 21, yards: 209, touchdowns: 2 }, { player: "Hud Price", schoolSlug: "de-leon", attempts: 5, yards: 50, touchdowns: 2 }, { player: "Beau Morris", schoolSlug: "de-leon", attempts: 6, yards: 49 }, { player: "Ed Garcia", schoolSlug: "de-leon", attempts: 12, yards: 43 }, { player: "Bryce Burkeen", schoolSlug: "de-leon", attempts: 1, yards: 26 },
-      { player: "Miles Follis", schoolSlug: "stamford", attempts: 7, yards: 18 }, { player: "Brenham Walker", schoolSlug: "stamford", attempts: 6, yards: 9 }, { player: "Slayden Young", schoolSlug: "stamford", attempts: 2, yards: 6 }, { player: "Chris McCann", schoolSlug: "stamford", attempts: 2, yards: 2 }, { player: "Josh Andrew", schoolSlug: "stamford", attempts: 2, yards: -1 },
+      { player: "Miles Follis", schoolSlug: "stamford", attempts: 7, yards: 18 }, { player: "Brenham Walker", schoolSlug: "stamford", attempts: 6, yards: 9 }, { player: "Slayden Young", schoolSlug: "stamford", attempts: 2, yards: 6 }, { player: "Chris McCann", schoolSlug: "stamford", attempts: 2, yards: 2 }, { player: "Josh Andruch", schoolSlug: "stamford", attempts: 2, yards: -1 },
     ],
     passing: [
       { player: "Hud Price", schoolSlug: "de-leon", completions: 10, attempts: 17, yards: 99, interceptions: 0, touchdowns: 3 }, { player: "Beau Morris", schoolSlug: "de-leon", completions: 0, attempts: 2, yards: 0, interceptions: 0 }, { player: "Miles Follis", schoolSlug: "stamford", completions: 19, attempts: 36, yards: 283, interceptions: 3 },
