b17c1a3be55c360fbb70017a61755e0c708a1d11
varsityvue <info@varsityvue.com>
2026-09-08 23:29:00 -0500

    Wire Santo program logo

--- components/SchoolBadge.tsx
+++ components/SchoolBadge.tsx
@@ -14,6 +14,7 @@ const programLogoBySlug: Record<string, string> = {
   albany: "/logos/schools/albany.png",
   stamford: "/logos/schools/stamford.png",
   stephenville: "/logos/schools/stephenville.png",
+  santo: "/logos/schools/santo.png",
 };
 
 export function getProgramLogoPath(schoolSlug: string) {
