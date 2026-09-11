1a816e893d2a9e92b5951c1ae247aeafbc57f818
varsityvue <info@varsityvue.com>
2026-09-11 00:36:13 -0500

    Add optional article featured image fields

--- types/platform.ts
+++ types/platform.ts
@@ -199,6 +199,9 @@ export type Article = {
   subtitle?: string;
   excerpt: string;
   body: string;
+  featuredImageUrl?: string;
+  featuredImageAlt?: string;
+  featuredImageCaption?: string;
 
   type: ArticleType;
 
