b4800adcce16164e1d95145d94fe3857ef676d8a
varsityvue <info@varsityvue.com>
2026-09-12 02:18:08 -0500

    Use charcoal VarsityVue V app icon

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,41 +1,22 @@
 import { ImageResponse } from "next/og";
 
-export const size = {
-  width: 512,
-  height: 512,
-};
-
+export const size = { width: 512, height: 512 };
 export const contentType = "image/png";
-
-const outerPath =
-  "M 47 49 L 46 194 L 99 195 L 282 678 L 592 678 L 784 195 L 836 194 L 835 47 L 499 48 L 499 194 L 542 207 L 444 447 L 340 207 L 384 193 L 384 48 Z";
-const redPath =
-  "M 76 76 L 75 167 L 122 170 L 304 649 L 570 648 L 758 177 L 806 164 L 807 76 L 527 77 L 527 167 L 587 179 L 445 523 L 423 514 L 299 167 L 355 166 L 355 76 Z";
 
 export default function Icon() {
   return new ImageResponse(
-    (
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          background: "#05070A",
-        }}
-      >
-        <svg
-          width="450"
-          height="362"
-          viewBox="0 0 883 710"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d={outerPath} fill="#F3E9D5" stroke="#111111" strokeWidth="32" strokeLinejoin="round" />
-          <path d={redPath} fill="#C8102E" stroke="#111111" strokeWidth="8" strokeLinejoin="round" />
+    <div style={{ width: "100%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center", background: "#252729" }}>
+      <div style={{ width: 430, height: 430, display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <svg width="430" height="430" viewBox="0 0 430 430" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <filter id="shadow" x="-20%" y="-20%" width="140%" height="140%"><feDropShadow dx="0" dy="8" stdDeviation="7" floodColor="#000" floodOpacity="0.45"/></filter>
+          </defs>
+          <path filter="url(#shadow)" fill="#080808" d="M35 42h143l37 94 37-94h143v77h-57L258 388h-86L92 119H35z"/>
+          <path fill="#F1E5CF" d="M49 55h119l47 119 47-119h119v50h-53L247 374h-64L102 105H49z"/>
+          <path fill="#B20D22" stroke="#080808" strokeWidth="8" d="M63 69h96l56 143 56-143h96v22h-50L236 360h-42L113 91H63z"/>
         </svg>
       </div>
-    ),
+    </div>,
     size
   );
 }
