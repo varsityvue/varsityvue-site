205315a4bccfab379762a7580d91e3c3ad9a663c
varsityvue <info@varsityvue.com>
2026-09-12 23:41:39 -0500

    Keep internal tools out of search

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
+        disallow: ["/internal/"],
       },
     ],
     sitemap: "https://varsityvue.com/sitemap.xml",
